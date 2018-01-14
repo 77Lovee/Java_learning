1a3e5069b5c6164bf4caf8e8b6931afb82428509
77Lovee <2805431021@qq.com>
2018-01-14 19:41:07 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello Java");
+    }
+   }
+   
+   //file name shoud be same with the class you create, like Hello.java file, it contain a class Hello.
