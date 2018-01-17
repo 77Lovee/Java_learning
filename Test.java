e068094b74ef22061908a24d475e9c91fe69d1c5
77Lovee <2805431021@qq.com>
2018-01-17 05:13:11 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test{
+	
+	public static void mark(){
+	System.out.println("This program is finished");
+  }
+  
+  
+	public static void main(String[] args){
+		for(int i=1;i<12;i=i+1){
+		System.out.println("hello world!");
+	}
+		mark();
+}
+}
+
+//class finished{
+	
+//}
